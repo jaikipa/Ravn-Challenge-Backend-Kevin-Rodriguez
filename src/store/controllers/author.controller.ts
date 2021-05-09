2be53a4be554b06c3391a03d56ac877835696e1a
Kevin Escoto <krodriguez@applaudostudios.com>
2021-05-09 00:04:53 -0600

    :zap: chore: add caching layer

--- src/store/controllers/author.controller.ts
+++ src/store/controllers/author.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, CacheInterceptor, UseInterceptors } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthorFilterDto } from '../dtos/request/author-filter.dto';
 import { AuthorResponseDto } from '../dtos/response/author-response.dto';
@@ -13,6 +13,7 @@ export class AuthorController {
     summary: 'Retrieve Authors',
     description: 'Use for retrieve authors and search author by name',
   })
+  @UseInterceptors(CacheInterceptor)
   @Get('')
   findAuthors(@Query() authorFilterDto: AuthorFilterDto): Promise<AuthorResponseDto[]> {
     return this.authorService.findAuthors(authorFilterDto);
--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorController } from './controllers/author.controller';
 import { SaleItemRepository } from './repositories/sale-item.repository';
 import { AuthorService } from './services/author.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SaleItemRepository])],
+  imports: [
+    TypeOrmModule.forFeature([SaleItemRepository]),
+    CacheModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        store: redisStore,
+        host: config.get('REDIS_HOST'),
+        port: config.get('REDIS_PORT'),
+        ttl: config.get('REDIS_TTL'),
+      }),
+    }),
+  ],
   controllers: [AuthorController],
   providers: [AuthorService],
 })
