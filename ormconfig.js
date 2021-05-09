eb681e1b18c1389603d163afef5c3c8041da20b7
Kevin Escoto <krodriguez@applaudostudios.com>
2021-05-08 22:49:04 -0600

    🔧 chore: add docker files

--- ormconfig.js
+++ ormconfig.js
@@ -11,7 +11,7 @@ module.exports = [
     password: process.env.ORM_PASSWORD,
     schema: process.env.ORM_SCHEMA,
     database: process.env.ORM_DATABASE,
-    entities: [__dirname + '/**/*.entity.{js,ts}'],
+    entities: [__dirname + '/**/*.entity.js'],
     migrations: [process.env.ORM_MIGRATIONS],
     migrationsTableName: [process.env.ORM_MIGRATIONS_TABLE_NAME],
     logging: true,
@@ -19,22 +19,7 @@ module.exports = [
     cli: {
       migrationsDir: process.env.ORM_MIGRATIONS_DIR,
     },
-  },
-  {
-    name: 'test',
-    type: process.env.ORM_CONNECTION,
-    host: process.env.ORM_HOST,
-    username: process.env.ORM_USERNAME,
-    password: process.env.ORM_PASSWORD,
-    port: parseInt(process.env.ORM_PORT, 10),
-    database: process.env.ORM_TEST_DATABASE,
-    schema: process.env.ORM_SCHEMA,
-    entities: [process.env.ORM_ENTITIES],
-    migrations: [process.env.ORM_MIGRATIONS],
-    migrationsTableName: [process.env.ORM_MIGRATIONS_TABLE_NAME],
-    migrationsRun: true,
-    cli: {
-      migrationsDir: process.env.ORM_MIGRATIONS_DIR,
-    },
+    seeds: [__dirname + '/**/seeds/*.js'],
+    factories: [__dirname + '/**/factories/*.js'],
   },
 ];
